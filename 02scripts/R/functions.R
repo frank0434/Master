c14d15b482a787d1fbd5de4aa8f2e242189e9c9d
jian liu <jian.liu@plantandfood.co.nz>
2020-03-06 23:56:52 +1300

    over write the db by new checkings

--- 02scripts/R/functions.R
+++ 02scripts/R/functions.R
@@ -1,5 +1,38 @@
 
 
+#' read_met_col
+#'
+#' @description read met col names only
+#' 
+#' @param path 
+#' @param skip 
+#' @param nrows 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+read_met_col <- function(path = path_met, skip = 7){
+  met_col <- data.table::fread(input = path, skip = skip, nrows = 1)
+  met_col
+}
+
+
+#' Title
+#'
+#' @param path 
+#' @param skip 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+read_met <- function(path = path_met, skip_unit = 9, skip_meta = 7){
+  met_LN <- data.table::fread(input = path,skip = skip_unit, fill = TRUE)
+  met_col <- read_met_col(path = path, skip = skip_meta)
+  colnames(met_LN) <- colnames(met_col)
+  return(met_LN)
+}
 
 #' exam_xlsxs
 #' 
