fcd46fea9bb757f19281d7076d5e056121e42c9a
frank0434 <jian.liu@plantandfood.co.nz>
2020-05-16 00:16:24 +1200

    Curve fitting to extract kl failure - only 222 of 460

--- 02scripts/R/20200515curveFittingKLs.R
+++ 02scripts/R/20200515curveFittingKLs.R
@@ -3,28 +3,27 @@
 # Depends on .2_Data_EAD.Rmd
 
 
-PROBLEM1 <- nested[Experiment =="Iversen12" & SowingDate == "SD1" & variable == "SW(3)"]$data[[1]]
-PROBLEM1 %>% 
+PROBLEM1 <- nested[Experiment =="Iversen12" & SowingDate == "SD10" ]
+PROBLEM1[, unlist(data, recursive = FALSE), by = .(Experiment, variable)]%>% 
   ggplot(aes(Clock.Today, value)) + 
-  geom_point()
-ll.0 =  min(PROBLEM1$value)/2
+  geom_point() +
+  facet_grid(variable ~ .)
+test <- PROBLEM1[, unlist(data, recursive = FALSE), by = .(Experiment, variable)][variable=="SW(1)"]
+
+ll.0 =  min(test$value)/2
 # Use a simple liner model to get the starting points for the decay rate _kl_
 # The liner model has to be log transferred first 
-model.0 = lm(log(value - ll.0) ~ t, data = PROBLEM1)  
+model.0 = lm(log(value - ll.0) ~ t, data = test)  
 pawc.0 = exp(coef(model.0)[1]) # The starting point for interception
 kl.0 = coef(model.0)[2] # kl starting point
 #  Starting parameters
 start = list(PAWC = pawc.0, kl = kl.0, LL = ll.0)
 tryCatch({
-  model = nls(value ~ PAWC * exp(kl * t) + LL , data = PROBLEM1, start = start)
+  model = nls(value ~ PAWC * exp(kl * t) + LL , data = test, start = start)
 },
 error = function(cond){
   message("nls can't describe the data")
   message(cond)
   return(NA)
-}, 
-message =  function(cond){
-  message("nls can't describe the data")
-  message(cond)
 })
 broom::tidy(model)
