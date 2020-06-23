e24cf84e6ab874f8b6ad2dc3d56a980b1d80fac2
frank0434 <jian.liu@plantandfood.co.nz>
2020-06-24 08:29:10 +1200

    Update the plan based on the fixes

--- 02scripts/R/plan_LayerCalibr.R
+++ 02scripts/R/plan_LayerCalibr.R
@@ -6,35 +6,51 @@ plan_LayerCalibr <- drake::drake_plan(
                          ][order(NSE, R2,RMSE)
                            ][, .SD[1], by = .(Experiment, SowingDate)],
   info = best_fit[,.(Experiment, SowingDate, SimulationID, SKL, KLR,RFV)],
-  # SW_DUL_LL = readd(SW_DUL_LL),
-  # SD_tidied = readd(SD_tidied),
-  files = sapply(seq(1, nrow(info)), function(x){
+
+  best_files = lapply(seq(1, nrow(info)), function(x){
     orig = here::here("03processed-data/apsimxFiles/")
-    copyto = here::here("03processed-data/apsimxFilesLayers/")
     basename = paste0("ModifiedSKL_", 
                       info[x, ][["SKL"]],
                       info[x, ][["Experiment"]], 
-                      info[x, ][["SowingDate"]], ".apsimx")
+                      info[x, ][["SowingDate"]], ".db")
     file = file.path(orig, basename)
-    system(paste("cp", file, copyto))
-  })
+    dt = read_dbtab(file, "SummaryNumbers")
+    dt[, ':='(Experiment = info[x, ][["Experiment"]],
+              SowingDate = info[x, ][["SowingDate"]],
+              SKL = info[x, ][["SKL"]])]
+    
+    dt = dt[SimulationID == info[x, ][["SimulationID"]]]
+  }), 
+  kls = process_list(best_files), 
+  
+  # files = sapply(seq(1, nrow(info)), function(x){
+  #   orig = here::here("03processed-data/apsimxFiles/")
+  #   copyto = here::here("03processed-data/apsimxFilesLayers/")
+  #   basename = paste0("ModifiedSKL_", 
+  #                     info[x, ][["SKL"]],
+  #                     info[x, ][["Experiment"]], 
+  #                     info[x, ][["SowingDate"]], ".apsimx")
+  #   file = file.path(orig, basename)
+  #   system(paste("cp", file, copyto))
+  # })
 
   ## Prepare the configuration file and create multiple slurp simulations 
   # .2_Data_EDA_Part2_apsimxEdit.Rmd
   # Set up initial condtions 
-  # apsimxs = target(
-  #   EditApsimxLayers(apsimx, best_fit, SW_DUL_LL, SD_tidied),
-  #   trigger = trigger(condition =  FALSE)
-  # ),
+  apsimxs = target(
+    EditApsimxLayers(apsimx, info, readd(SW_DUL_LL), readd(SD_tidied), kls),
+    trigger = trigger(condition =  FALSE)
+  ),
 
-  # files = list.files("./03processed-data/apsimxFilesLayers/", "^Modif.+.apsimx$", full.names = TRUE),
+  files = list.files(here::here("03processed-data/apsimxFilesLayers/"), 
+                     pattern = "^Layer.+.apsimx$", full.names = TRUE),
   # Edit layer from 2 and below
-  # apsimxlayerkl = target(
-  #   EditLayerKL_multi(layer = layer, KL_range, path =  apsimx, 
-  #                     files = files[1:18],
-  #                     saveTo = path_sims2),
-  #   trigger = trigger(condition =  FALSE)
-  # )
+  apsimxlayerkl = target(
+    EditLayerKL_multi(KL_layers , KL_range, path =  apsimx,
+                      files = files[1:18],
+                      saveTo = path_sims2),
+    trigger = trigger(condition =  TRUE)
+  )
   # l_stats_layerKL = autoapsimx::sims_stats_multi(path_sims = "./03processed-data/apsimxFilesLayers/",
   #                                                pattern = "^SKL.+.db$", 
   #                                                DT_observation = readd(SW_mean),
