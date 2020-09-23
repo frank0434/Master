dd61aff4511a0969bc6df63d4620d58931cc776c
frank0434 <jian.liu@plantandfood.co.nz>
2020-09-24 09:14:24 +1200

    Place the group in season fun into function.R

--- 04manuscript/DrawFigures/locationMap.R
+++ 04manuscript/DrawFigures/locationMap.R
@@ -27,28 +27,6 @@ mapview::mapshot(locationMap, file = "04manuscript/locationmap.png",
 
 
 
-# met file summary --------------------------------------------------------
-source("./02scripts/R/packages.R")
-path = "./03processed-data/Richard.sqlite3"
-
-AD = read_dbtab(path, "met_AshleyDene")
-
-I12 = read_dbtab(path = path, "met_Iversen12")
-AD[, Clock.Today := as.Date(day, origin = paste0(year,"-01-01"))]
-AD$Clock.Today %>% range()
-I12 = I12[, Clock.Today := as.Date(day, origin = paste0(year,"-01-01"))
-          ][Clock.Today %between% range(AD$Clock.Today)]
-AD[, seasons := ifelse(Clock.Today >= "2010-06-02" & Clock.Today <= "2011-06-01",
-                       "2010/11", 
-                       ifelse(Clock.Today >= "2011-06-02" & Clock.Today <= "2012-06-01",
-                              "2011/12", "2012"))]
-I12[, seasons := ifelse(Clock.Today >= "2010-06-02" & Clock.Today <= "2011-06-01",
-                       "2010/11", 
-                       ifelse(Clock.Today >= "2011-06-02" & Clock.Today <= "2012-06-01",
-                              "2011/12", "2012"))]
-AD[, .(AnnualRain = sum(rain, na.rm = TRUE)), by = .(seasons)]
-I12[, .(AnnualRain = sum(rain, na.rm = TRUE)), by = .(seasons)]
-
 # PET ---------------------------------------------------------------------
 
 read_excel("./04manuscript/refData/wgenf.xlsx")
--- Scripts/R/functions.R
+++ Scripts/R/functions.R
@@ -1,6 +1,79 @@
 
 
 
+
+#' chop_dates
+#'
+#' @description Chop a date object into year, yearMonth and monthDay
+#' 
+#' @param DT a data.table
+#' @param col_date a string for the date object column
+#'
+#' @return
+#' @export
+#'
+#' @examples
+chop_dates <- function(DT, col_date = "Date"){
+  DT[, ':='(Year = gsub("-\\d{2}-\\d{2}", "", get(col_date)),
+            YearMonth = gsub("-\\d{2}$", "", get(col_date)),
+            MonthDay = gsub("\\d{4}-", "", get(col_date)))]
+  return(DT)
+}
+
+#' group_in_season
+#' @description  Group annual data into seasonal data 1 July to 30 June next year
+#' 
+#'
+#' @param DT, data.table
+#'
+#' @return data.table with a column `Season`
+#' @export
+#'
+#' @examples
+group_in_season <- function(DT){
+  
+  stopifnot("Date" %in% colnames(DT))
+  
+  period <- range(DT$Date)
+  noofyear <- diff.Date(period, unit = "year") %>% 
+    as.numeric(.)/365
+  startyear <- year(period[1])
+  endyear <- year(period[2])
+  startmd <- "-07-01"
+  endmd <- "-06-30"
+  noofseason <- round(noofyear, digits = 0)
+  
+  # Initial a vector to store the text as cmd
+  cmd <- vector("character", noofseason)
+  
+  # Build a cmd to do conditional evaluation 
+  
+  for(i in 0:(noofseason-1)){
+    # Key condition
+    v <- paste0("Date >= \"" , startyear + i, startmd,"\"","&",
+                "Date <= \"", startyear + i + 1, endmd, "\"",",",
+                "\"", startyear +i,"/", startyear + i + 1, "\"",",")
+    # Check the format 
+    # cat(v)
+    # Store it; must be i + 1 since R has no 0 position
+    cmd[i + 1] <- v
+  }
+  # Collapse into one string and glue the fcase function 
+  cmd <- paste0("fcase( ", paste(cmd, collapse = ""), ")")
+  
+  # Delete the end comma
+  cmd <- gsub(",)$", ")", cmd)
+  # Check format again
+  cat("Check if the command format is correct\r\n", cmd)
+  
+  DT[, Season:= eval(parse(text = cmd))]
+  return(DT)
+  
+  
+}
+
+
+
 process_bestfit <- function(skl_best_fit, kl_best_fit, saveTo){
   skl_best_fit = skl_best_fit[,.(Experiment, SowingDate, kl = SKL, Depth = 1L)]
   kl_best_fit[,kl := as.numeric(gsub("kl","", kl))]
