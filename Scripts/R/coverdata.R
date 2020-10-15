8325975c1d4748c7ea1a2e644488a2b1242f0203
frank0434 <jian.liu@plantandfood.co.nz>
2020-10-16 07:18:04 +1300

    Convert py script to R

---
+++ Scripts/R/coverdata.R
@@ -0,0 +1,55 @@
+path_richard = "c:/Users/cflfcl/Dropbox/Data/APSIM_Sim.xlsx"
+loadd(sowingDates)
+loadd(starts_with("met"))
+
+biomass <- read_Sims(path = path_richard, source = "biomass")
+
+biomass_cols <- c('Experiment', 'Clock.Today', 'SowingDate', 'Rep',
+                  'Plot', 'Rotation.No.', 'Harvest.No.', 'Height','LAImod')
+
+LAI_Height <-  biomass[Seed== 'CS' & Harvest.No.!= "Post"
+                       ][,..biomass_cols
+                         ][, unlist(list(lapply(.SD, mean, na.rm = TRUE)), recursive = FALSE),
+                           by = .(Experiment, SowingDate, Clock.Today),
+                           .SDcols = c("Height", "LAImod")]
+LAI_Height_SD <- merge.data.table(LAI_Height, sowingDates, 
+                                  by = c("Experiment", "Clock.Today" , "SowingDate"), 
+                                  all= TRUE)
+LAI_Height_SD[, ':='(LAImod = ifelse(is.na(LAImod), 0, LAImod),
+                     Height = ifelse(is.nan(Height), NA, Height),
+                     k = 0.94)
+              ][Experiment == "AshleyDene" & Clock.Today %between% c( '2011-11-30','2012-03-01'),
+                k:= 0.66][, LI := 1 - exp(-k * LAImod) ]
+# LAI_Height_SD %>% 
+#   ggplot(aes(Clock.Today, k)) +
+#   geom_point() +
+#   facet_wrap(~ Experiment)
+LAI_wide <- dcast.data.table(LAI_Height_SD, 
+                             Experiment + Clock.Today ~ SowingDate, 
+                             value.var = "LAImod" )
+LI_wide <- dcast.data.table(LAI_Height_SD, 
+                             Experiment + Clock.Today ~ SowingDate, 
+                             value.var = "LI" )
+
+met <- rbindlist(list(met_Iversen12[, Experiment := "Iversen12"],
+                      met_AshleyDene), use.names = TRUE)
+accumTT <- met[,.(Experiment, Clock.Today, AccumTT)]
+DT <- merge.data.table(accumTT, LAI_wide, by = c("Experiment", "Clock.Today"), 
+                 all.x = TRUE)
+
+DT <- melt.data.table(data = DT, 
+                      id.vars = c("Experiment", "Clock.Today", "AccumTT"), 
+                      value.name = "LAI",
+                      variable.name = "SowingDate", variable.factor = FALSE)
+
+DT[, LAI:= na.approx(LAI, AccumTT, na.rm = FALSE) , by = .(Experiment, SowingDate) ]
+
+# slurp need LAI WITH k
+
+# plot(na.approx(DT$SD1, x = DT$AccumTT, na.rm = FALSE))
+DT %>% 
+  ggplot(aes(Clock.Today, LAI, color = SowingDate)) +
+  geom_point() +
+  geom_line()+
+  facet_wrap(~ Experiment)
+
