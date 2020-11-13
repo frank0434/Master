e095b5f0fc0098cf4d359b80a610c262c0aff0dc
frank0434 <jian.liu@plantandfood.co.nz>
2020-11-13 22:58:34 +1300

    Add initial SW calculation

--- _targets.R
+++ _targets.R
@@ -9,12 +9,18 @@ path_apsimx <- "C:/Data/ApsimX/ApsimXLatest/Bin/Models.exe"
 path_lincoln <- here::here("Data/ClimateAndObserved/lincoln.met")
 path_AD <- here::here("Data/ClimateAndObserved/AshleyDene.met")
 
-Sys.setenv("WorkingDir" = here::here("Data/"))
-Sys.setenv("BaseApsimxDir" = file.path(Sys.getenv("WorkingDir"), "ApsimxFiles/"))
-Sys.setenv("MetDir" = file.path(Sys.getenv("WorkingDir"), "ClimateAndObserved/"))
-Sys.setenv("ConfigFileDir" = file.path(Sys.getenv("WorkingDir"), "ProcessedData/ConfigurationFiles/"))
-Sys.setenv("CoverDataDir" = file.path(Sys.getenv("WorkingDir"), "ProcessedData/CoverData/"))
-Sys.setenv("SimsDir" = file.path(Sys.getenv("WorkingDir"), "ProcessedData/apsimxFiles/"))
+Sys.setenv("WorkingDir" = 
+             here::here("Data/"))
+Sys.setenv("BaseApsimxDir" = 
+             file.path(Sys.getenv("WorkingDir"), "ApsimxFiles/"))
+Sys.setenv("MetDir" = 
+             file.path(Sys.getenv("WorkingDir"), "ClimateAndObserved/"))
+Sys.setenv("ConfigFileDir" = 
+             file.path(Sys.getenv("WorkingDir"), "ProcessedData/ConfigurationFiles/"))
+Sys.setenv("CoverDataDir" = 
+             file.path(Sys.getenv("WorkingDir"), "ProcessedData/CoverData/"))
+Sys.setenv("SimsDir" = 
+             file.path(Sys.getenv("WorkingDir"), "ProcessedData/apsimxFiles/"))
 
 # Set target-specific options such as packages.
 tar_option_set(packages = c("data.table", "magrittr", "readxl", 
@@ -23,26 +29,43 @@ tar_option_set(packages = c("data.table", "magrittr", "readxl",
 
 # Define targets
 targets <- list(
+  # Define keys and treatments
+  tar_target(Sites, unique(CoverData$Experiment)),
+  tar_target(SD, unique(CoverData$SowingDate)),
   tar_target(id_vars, c("Experiment", "SowingDate", "Clock.Today")),
   tar_target(value_vars, grep("SW\\(\\d.+", colnames(data_SW), value = TRUE)),
+  
+  # Read data
   tar_target(data_SW, read_Sims(path = path_richard)),
   tar_target(sowingDates, read_Sims(path_richard, source =  "sowingDate")),
   tar_target(LAI_Height,  read_Sims(path = path_richard, source = "biomass")),
-  tar_target(Sites, unique(CoverData$Experiment)),
-  tar_target(SD, unique(CoverData$SowingDate)),
   tar_target(met_AshleyDene, read_met(path_AD, skip_unit = 10, skip_meta = 8)),
   tar_target(met_Iversen12, read_met(path_lincoln, skip_unit = 8, skip_meta = 6,
                                      site = "Iversen12")),
+  
+  # Do calculation
+  tar_target(SW_mean, colwise_meanSW(data_SW = data_SW, id.vars = id_vars, col.vars = value_vars)),
+  
   tar_target(cumTT, rbindlist(list(met_Iversen12, met_AshleyDene),
                               use.names = TRUE)[,.(Experiment, Clock.Today, AccumTT)]),
-  tar_target(observed, outputLAIobserved(biomass = LAI_Height,
+  tar_target(CoverData, interp_LAI(biomass = LAI_Height, sowingDates, cumTT)),
+  
+  tar_target(SW_initials, initialSWC(SW_mean, sowingDates, id_vars)),
+  # Output apsimx input and observed
+  tar_target(observed_LAI, outputLAIobserved(biomass = LAI_Height,
                                          Sites, SD,
                                          output = Sys.getenv("CoverDataDir")), 
              format = "file", 
              pattern = cross(Sites, SD), 
              cue = tar_cue(depend = TRUE)),
-  tar_target(CoverData, interp_LAI(biomass = LAI_Height, sowingDates, cumTT)),
-  tar_target(LAIinput, outputLAIinput(CoverData, Sites, SD, 
+  tar_target(observed_SW, outputSWobserved(SW = SW_mean,
+                                         Sites, SD,
+                                         output = Sys.getenv("CoverDataDir")), 
+             format = "file", 
+             pattern = cross(Sites, SD), 
+             cue = tar_cue(depend = TRUE)),
+  
+  tar_target(LAI_input, outputLAIinput(CoverData, Sites, SD, 
                                       output = Sys.getenv("CoverDataDir")),
              format = "file", 
              pattern = cross(Sites, SD), 
