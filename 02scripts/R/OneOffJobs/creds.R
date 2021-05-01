2dfe34240712c78b5fd28e07ddfe57c739f22d56
frank0434 <jian.liu@plantandfood.co.nz>
2021-05-01 23:19:05 +1200

    update envs

---
+++ 02scripts/R/OneOffJobs/creds.R
@@ -0,0 +1,13 @@
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+# P007_MaizeNZ_DEMO_DB <- list(dbname = "postgres",
+#                              host = "dbP007Demo",
+#                              username = "postgres",
+#                              password = "password")
+# P009_LucerneNZ_DEMO_DB <- list(dbname = "postgres",
+#                                host = "dbP009Demo",
+#                                username = "postgres",
+#                                password = "password")
